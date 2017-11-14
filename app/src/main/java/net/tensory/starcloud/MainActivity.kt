7861f7eaf14d046e5aeeaec27ac9e4de212f407d
Ari Lacenski <alacenski@gmail.com>
2017-11-14 11:58:21 -0800

    Project commit.

---
+++ app/src/main/java/net/tensory/starcloud/MainActivity.kt
@@ -0,0 +1,11 @@
+package net.tensory.starcloud
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+}
